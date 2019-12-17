93142af14b62d17cf3ccc7bd452e01d6937a6bee
Mateusz.rostkowsky995@gmail.com <mateusz.rostkowsky995@gmail.com>
2019-12-17 18:52:42 +0100

    refactor of the queryselectors

--- game.js
+++ game.js
@@ -1,140 +1,137 @@
-let box = document.querySelector("main"),
-        cop = document.querySelector(".police"),
-        thiev = document.querySelector(".thieves"),
-        msgBox = document.querySelector(".messages");
-        timer = document.querySelector(".timer");
-        start = document.querySelector("#startGame");
-           
-      
-      
-      // starting position after site loads
-      let copX = (box.offsetWidth - cop.offsetWidth)*(3/4);
-      let copY = (box.offsetHeight - cop.offsetHeight)/2;
-      let thievX = (box.offsetWidth - thiev.offsetWidth)*(1/4);
-      let thievY = (box.offsetHeight - cop.offsetHeight)/2;
-      // speed of players
-      let moveSpeed = 5;
+const box = document.querySelector("main");
+const cop = document.querySelector(".police");
+const thiev = document.querySelector(".thieves");
+const msgBox = document.querySelector(".messages");
+const timer = document.querySelector(".timer");
+const start = document.querySelector("#startGame");
 
-      let keys = [];
+// starting position after site loads
+let copX = (box.offsetWidth - cop.offsetWidth) * (3 / 4);
+let copY = (box.offsetHeight - cop.offsetHeight) / 2;
+let thievX = (box.offsetWidth - thiev.offsetWidth) * (1 / 4);
+let thievY = (box.offsetHeight - cop.offsetHeight) / 2;
+// speed of players
+let moveSpeed = 5;
 
-      function randomSpawn() {
-        copX = 400 + Math.floor(Math.random() * 36) * 10;
-        copY = Math.floor(Math.random() * 36) * 10;
-        thievX = Math.floor(Math.random() * 36) * 10;
-        thievY = Math.floor(Math.random() * 36) * 10;
-      }
+let keys = [];
 
-      function checkCollision() {
-        if (
-          cop.offsetTop < thiev.offsetTop + thiev.offsetWidth &&
-          cop.offsetTop + cop.offsetWidth > thiev.offsetTop &&
-          cop.offsetLeft < thiev.offsetLeft + thiev.offsetHeight &&
-          cop.offsetLeft + cop.offsetHeight > thiev.offsetLeft
-        ) {
-          gameOver("Police won");
-        }
-      }
+function randomSpawn() {
+  copX = 400 + Math.floor(Math.random() * 36) * 10;
+  copY = Math.floor(Math.random() * 36) * 10;
+  thievX = Math.floor(Math.random() * 36) * 10;
+  thievY = Math.floor(Math.random() * 36) * 10;
+}
 
-      let count;
+function checkCollision() {
+  if (
+    cop.offsetTop < thiev.offsetTop + thiev.offsetWidth &&
+    cop.offsetTop + cop.offsetWidth > thiev.offsetTop &&
+    cop.offsetLeft < thiev.offsetLeft + thiev.offsetHeight &&
+    cop.offsetLeft + cop.offsetHeight > thiev.offsetLeft
+  ) {
+    gameOver("Police won");
+  }
+}
 
-      // start Counting down when game starts
-      function startCount() {
-        clearInterval(count);
+let count;
 
-        timer.innerText = `00:30`;
-        let counter = 30;
-        
-        count = setInterval(function() {
-          counter--;
-          timer.innerText = `00:${counter}`;
-          if (counter <= 9) {
-            timer.innerText = `00:0${counter}`;
-          }
-          if (counter === -1) {
-            timer.innerText = "Game Ends";
-            gameOver("Thieves won");
-            clearInterval(count);
-          }
-        }, 1000);
-      }     
+// start Counting down when game starts
+function startCount() {
+  clearInterval(count);
 
-      // ends game
-      function gameOver(who) {
-        cop.classList.add("crashed");
-        thiev.classList.add("crashed");
-        console.log(cop.classList)
-        clearInterval(count);
+  timer.innerText = `00:30`;
+  let counter = 30;
 
-        keys = [];
+  count = setInterval(function() {
+    counter--;
+    timer.innerText = `00:${counter}`;
+    if (counter <= 9) {
+      timer.innerText = `00:0${counter}`;
+    }
+    if (counter === -1) {
+      timer.innerText = "Game Ends";
+      gameOver("Thieves won");
+      clearInterval(count);
+    }
+  }, 1000);
+}
 
-        document.removeEventListener("keydown", keyPressed);
-        document.removeEventListener("keyup", keyReleased);
-        timer.innerText = "Play again!";
-        if (who === "Thieves won") {
-          msgBox.innerText = who + "!";
-        } else if (who === "Police won") {
-          msgBox.innerText = who + "!";
-        }
-      }
+// ends game
+function gameOver(who) {
+  cop.classList.add("crashed");
+  thiev.classList.add("crashed");
+  console.log(cop.classList);
+  clearInterval(count);
 
-      function startGame() {
-        startCount();
-        randomSpawn();
+  keys = [];
 
-        msgBox.innerText = "Catch the thieve";
+  document.removeEventListener("keydown", keyPressed);
+  document.removeEventListener("keyup", keyReleased);
+  timer.innerText = "Play again!";
+  if (who === "Thieves won") {
+    msgBox.innerText = who + "!";
+  } else if (who === "Police won") {
+    msgBox.innerText = who + "!";
+  }
+}
 
-        cop.classList.remove("crashed");
-        thiev.classList.remove("crashed");
+function startGame() {
+  startCount();
+  randomSpawn();
 
-        document.addEventListener("keydown", keyPressed);
-        document.addEventListener("keyup", keyReleased);
-      }
+  msgBox.innerText = "Catch the thieve";
 
-      
-      function keyPressed(event) {
-        keys[event.keyCode] = true;
-      }
-      function keyReleased(event) {
-        keys[event.keyCode] = false;
-      }
+  cop.classList.remove("crashed");
+  thiev.classList.remove("crashed");
 
-      // 
-      function animate() {
-        cop.style.left = copX + "px";
-        cop.style.top = copY + "px";
-        thiev.style.left = thievX + "px";
-        thiev.style.top = thievY + "px";
+  document.addEventListener("keydown", keyPressed);
+  document.addEventListener("keyup", keyReleased);
+}
 
-        if (keys[37] && copX > 0) {
-          copX -= moveSpeed;
-        }
-        if (keys[38] && copY > 0) {
-          copY -= moveSpeed;
-        }
-        if (keys[39] && copX < box.offsetWidth - cop.offsetWidth) {
-          copX += moveSpeed;
-        }
-        if (keys[40] && copY < box.offsetHeight - cop.offsetHeight) {
-          copY += moveSpeed;
-        }
-        if (keys[65] && thievX > 0) {
-          thievX -= moveSpeed;
-        }
-        if (keys[87] && thievY > 0) {
-          thievY -= moveSpeed;
-        }
-        if (keys[68] && thievX < box.offsetWidth - thiev.offsetWidth) {
-          thievX += moveSpeed;
-        }
-        if (keys[83] && thievY < box.offsetHeight - thiev.offsetHeight) {
-          thievY += moveSpeed;
-        }
-        checkCollision();
-        console.log(keys)
+function keyPressed(event) {
+  keys[event.keyCode] = true;
+}
+function keyReleased(event) {
+  keys[event.keyCode] = false;
+}
 
-        requestAnimationFrame(animate);
-      }
+//
+function animate() {
+  cop.style.left = copX + "px";
+  cop.style.top = copY + "px";
+  thiev.style.left = thievX + "px";
+  thiev.style.top = thievY + "px";
 
-      animate();
+  if (keys[37] && copX > 0) {
+    copX -= moveSpeed;
+  }
+  if (keys[38] && copY > 0) {
+    copY -= moveSpeed;
+  }
+  if (keys[39] && copX < box.offsetWidth - cop.offsetWidth) {
+    copX += moveSpeed;
+  }
+  if (keys[40] && copY < box.offsetHeight - cop.offsetHeight) {
+    copY += moveSpeed;
+  }
+  if (keys[65] && thievX > 0) {
+    thievX -= moveSpeed;
+  }
+  if (keys[87] && thievY > 0) {
+    thievY -= moveSpeed;
+  }
+  if (keys[68] && thievX < box.offsetWidth - thiev.offsetWidth) {
+    thievX += moveSpeed;
+  }
+  if (keys[83] && thievY < box.offsetHeight - thiev.offsetHeight) {
+    thievY += moveSpeed;
+  }
+  checkCollision();
+  console.log(keys);
 
-      start.addEventListener("click", startGame);
+  requestAnimationFrame(animate);
+}
+
+animate();
+
+start.addEventListener("click", startGame);
